27303b57715753fc0651d92ea9f75082306491e1
Blake Winton <bwinton@latte.ca>
2015-09-15 13:39:06 -0700

    See if the tab exists before creating a new one.

--- background.js
+++ background.js
@@ -6,6 +6,12 @@ chrome.browserAction.setBadgeText({text: '(ツ)'});
 chrome.browserAction.setBadgeBackgroundColor({color: '#eae'});
 
 chrome.browserAction.onClicked.addListener(function(aTab) {
-  chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
+  chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/'}, (tabs) => {
+    if (tabs.length === 0) {
+      // There is no catgif tab!
+      chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
+    } else {
+      // Do something here…
+    }
+  });
 });
-
