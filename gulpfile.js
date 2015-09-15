33f2a8f4c1dec243db8f33098174ab6bdf201aa9
Blake Winton <bwinton@latte.ca>
2015-09-15 11:27:32 -0700

    Use gulp to create the xpi.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var zip = require('gulp-zip');
 
 var files = ['manifest.json', 'background.js'];
 var xpiName = 'catgifs.xpi';
 
 gulp.task('default', function () {
-  console.log(files, xpiName)
+  gulp.src(files)
+    .pipe(zip(xpiName))
+    .pipe(gulp.dest('.'));
 });
+
